2dccd298397dd783e037b51d1fdb72273231f863
BielXL <gabrielvitornordt@gmail.com>
2025-07-21 16:50:39 -0300

    "end project"

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,31 +1,3 @@
-# import os
-# import ssl
-# from typing import AsyncGenerator
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# ssl_context = ssl.create_default_context()
-# ssl_context.check_hostname = False
-# ssl_context.verify_mode = ssl.CERT_NONE
-
-# connect_args = {
-#     "ssl": ssl_context
-# }
-
-# engine = create_async_engine(
-#     DATABASE_URL,
-#     echo=True,
-#     connect_args=connect_args
-# )
-
-# async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-
-# async def get_session() -> AsyncGenerator[AsyncSession, None]:
-#     async with async_session() as session:
-#         yield session
-
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import DATABASE_URL
